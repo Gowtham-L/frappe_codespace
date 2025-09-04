dba698db6b47b057db3973e3639bbc8b7524c610
GowthamL <45562756+Gowtham-L@users.noreply.github.com>
2025-09-04 15:09:09 +0530

    Update init.sh
    fix: yarn install added and bench Redis config url bug fixed

--- scripts/init.sh
+++ scripts/init.sh
@@ -18,6 +18,8 @@ nvm use 18
 echo "nvm use 18" >> ~/.bashrc
 cd /workspace
 
+npm i yarn
+
 bench init \
 --ignore-exist \
 --skip-redis-config-generation \
@@ -27,9 +29,12 @@ cd frappe-bench
 
 # Use containers instead of localhost
 bench set-mariadb-host mariadb
-bench set-redis-cache-host redis-cache:6379
-bench set-redis-queue-host redis-queue:6379
-bench set-redis-socketio-host redis-socketio:6379
+#bench set-redis-cache-host redis-cache:6379
+bench set-config -g redis_cache "redis://redis-cache:6379"
+#bench set-redis-queue-host redis-queue:6379
+bench set-config -g redis_queue "redis://redis-queue:6379"
+#bench set-redis-socketio-host redis-socketio:6379
+bench set-config -g redis_socketio "redis://redis-queue:6379"
 
 # Remove redis from Procfile
 sed -i '/redis/d' ./Procfile
